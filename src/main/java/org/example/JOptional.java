61ab21e2f2b4e24feae29589a8326efc2b53911c
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-21 14:18:51 -0400

    Finished implemenatation

--- src/main/java/org/example/JOptional.java
+++ src/main/java/org/example/JOptional.java
@@ -1,31 +1,46 @@
 package org.example;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class JOptional<T> {
+
+    private final T value;
+
+    private JOptional(T t) {
+        value = t;
+    }
+
     public static <T> JOptional<T> ofNullable(T t) {
-        //
+        return new JOptional<>(t);
     }
 
     public <U> JOptional<U> map(Function<T, U> mapper) {
-
+        return new JOptional<U>(mapper.apply(value));
     }
 
     public Stream<T> stream() {
-
+        return Stream.of(this.get());
     }
 
-    public T get() {
-
+    public T get() throws NoSuchElementException {
+        if (value == null)
+            throw new NoSuchElementException();
+        return value;
     }
 
     public T orElse(T backup) {
-
+        if (value == null)
+            return backup;
+        return value;
     }
 
     public T orElseGet(Supplier<T> backupCallback) {
-
+        if (value == null)
+            return backupCallback.get();
+        return value;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
 
-public class Main {
+import java.util.stream.Stream;
+
+public class Main<T> {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JOptional<String> emptyOptional = JOptional.ofNullable(null);
+        System.out.println(emptyOptional.orElse("I am an empty optional"));
+
+        JOptional<String> jOptional = JOptional.ofNullable("I am a string optional");
+        Stream<String> stringStream = jOptional.stream();
+        stringStream.forEach(System.out::println);
+
+        JOptional<Account> accountOptional = JOptional.ofNullable(new Account());
+        JOptional<Address> addressOptional = accountOptional.map(Account::getCustomer)
+                .map(Customer::getAddress);
+
+        System.out.println(addressOptional.orElse(new Address()));
+        System.out.println(addressOptional.orElseGet(Address::new));
+
     }
+}
+
+class Account {
+    Customer customer = new Customer();
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+}
+
+class Customer {
+    Address address = new Address();
+
+    public Address getAddress() {
+        return address;
+    }
+
+}
+
+class Address {
+
+    String street = "111 Address St";
+
+    Address() {
+
+    }
+
+    @Override
+    public String toString() {
+        return street;
+    }
+
 }
